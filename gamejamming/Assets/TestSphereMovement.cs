816411873e8091313329a6dcd130e12263f342e6
Dawn Felstar <thelegotechhead@yahoo.com.au>
2023-06-27 11:42:13 +1000

    mULTIPLAYER works

--- gamejamming/Assets/TestSphereMovement.cs
+++ gamejamming/Assets/TestSphereMovement.cs
@@ -1,24 +1,26 @@
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class TestSphereMovement : MonoBehaviour
+public class TestSphereMovement : NetworkBehaviour
 {
     [SerializeField] private float movementSpeed;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Vector3 lookAngle = new Vector3(60, 0, 0);
     [SerializeField] private Vector3 lookPosition = new Vector3(0, 20, -10);
+    bool enableInput = false;
     // Start is called before the first frame update
 
     public static List<TestSphereMovement> players = new List<TestSphereMovement>();
 
-    void Awake()
+    public override void OnNetworkSpawn()
     {
         players.Add(this);
-        transform.position.Set(1.1f, transform.position.y, transform.position.z);
-        PlayerInput network = GetComponent<PlayerInput>();
-        if (network != null)
+   
+        if (IsOwner && IsClient)
         {
+            enableInput = true;
             print("Network Found");
             Camera[] cameras = FindObjectsOfType<Camera>();
             if (cameras.Length > 0)
@@ -46,6 +48,7 @@ public class TestSphereMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!enableInput) { return; }
         float xInput = Input.GetAxisRaw("Horizontal");
         float zInput = Input.GetAxisRaw("Vertical");
         rb.velocity = new Vector3(xInput * movementSpeed, rb.velocity.y, zInput * movementSpeed);
